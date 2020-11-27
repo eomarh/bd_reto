debdb06bd7eca13b57e90db82ee3cfa8b24d3ad6
Omar Herrera <ing.omaar.h@gmail.com>
2020-11-27 14:17:51 -0600

    First commit

---
+++ test.py
@@ -0,0 +1,22 @@
+import mysql.connector
+import xml.etree.ElementTree as ET
+import xml2dict, dicttoxml
+
+#conexion-datos
+cnx = mysql.connector.connect(host="127.0.0.1", user="root", passwd="12345", db="bd_reto")
+exe = cnx.cursor(dictionary=True)
+
+#codigos
+query = "SELECT * FROM oc"
+exe.execute(query)
+filas = exe.fetchall()
+
+cnx.close()
+
+#Archivo XML
+ruta = "E:/C disk/Local C/Documentos/Universidad/5° Semestre/Repos 5°/Bases de datos/proyecto/Reto"
+
+xml = dicttoxml.dicttoxml(filas, attr_type=False)
+tree = ET.ElementTree(ET.fromstring(xml))
+
+tree.write(ruta+'/ejemplo.xml')
