c022044e9d529aa7afe43cd887364e709a772f58
Omar Herrera <ing.omaar.h@gmail.com>
2020-11-27 14:38:13 -0600

    New format

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ cnx.close()
 #Archivo XML
 ruta = "E:/C disk/Local C/Documentos/Universidad/5° Semestre/Repos 5°/Bases de datos/proyecto/Reto"
 
-xml = dicttoxml.dicttoxml(filas, attr_type=False)
+xml = dicttoxml.dicttoxml(filas, custom_root='productos', attr_type=False)
 tree = ET.ElementTree(ET.fromstring(xml))
 
 tree.write(ruta+'/ejemplo.xml')
