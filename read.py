6a25a2df3648f8bdeecfa8174aa853706cdf0e8c
Omar Herrera <ing.omaar.h@gmail.com>
2020-11-28 12:48:06 -0600

    Connection validate

--- read.py
+++ read.py
@@ -1,29 +1,42 @@
+from mysql.connector import errorcode
 import mysql.connector
 import xml2dict
 
 #DB
-cnx = mysql.connector.connect(host="127.0.0.1", user="root", passwd="12345", db="bd_reto")
-exe = cnx.cursor(dictionary=True)
+try:
+    cnx = mysql.connector.connect(host="127.0.0.1", user="root", passwd="12345", db="bd_reto")
+except mysql.connector.Error as er:
+    if er.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Usuario o contraseña incorrecto.")
+        print('')
+    elif er.errno == errorcode.ER_BAD_DB_ERROR:
+        print("No existe la Base de Datos indicada.")
+        print('')
+    else:
+        print(er)
+else:
+    exe = cnx.cursor(dictionary=True)
 
-#XML to dict
-xml=open('output.xml')
-xmldict = xml2dict.parse(xml.read())
+    #XML to dict
+    xml=open('output.xml')
+    xmldict = xml2dict.parse(xml.read())
 
-print('--Valores a insertar--')
-for item in xmldict["productos"]["item"]:
-    print(item["id_oc"],item["proveedor"],item["producto"],item["descproducto"],item["cantidad"], item["fechaentrega"], item["precio"])
-    #Asignacion a variables
-    data = {
-        'id_oc': int(item["id_oc"]),
-        'proveedor': item["proveedor"],
-        'producto': item["producto"],
-        'descproducto': item["descproducto"],
-        'cantidad': int(item["cantidad"]),
-        'fechaentrega': item["fechaentrega"],
-        'precio': float(item["precio"])
-    }
-    query = ("INSERT INTO oc_2(id_oc, proveedor, producto, descproducto, cantidad, fechaentrega, precio) VALUES(%(id_oc)s, %(proveedor)s, %(producto)s, %(descproducto)s, %(cantidad)s, %(fechaentrega)s, %(precio)s)")
-    exe.execute(query, data)
-    cnx.commit()
+    print('--Valores a insertar--')
+    for item in xmldict["productos"]["item"]:
+        print(item["id_oc"],item["proveedor"],item["producto"],item["descproducto"],item["cantidad"], item["fechaentrega"], item["precio"])
+        #Asignacion a variables
+        data = {
+            'id_oc': int(item["id_oc"]),
+            'proveedor': item["proveedor"],
+            'producto': item["producto"],
+            'descproducto': item["descproducto"],
+            'cantidad': int(item["cantidad"]),
+            'fechaentrega': item["fechaentrega"],
+            'precio': float(item["precio"])
+        }
+        #Ejecucion de QUERIES
+        query = ("INSERT INTO oc_2(id_oc, proveedor, producto, descproducto, cantidad, fechaentrega, precio) VALUES(%(id_oc)s, %(proveedor)s, %(producto)s, %(descproducto)s, %(cantidad)s, %(fechaentrega)s, %(precio)s)")
+        exe.execute(query, data)
+        cnx.commit()
 
-cnx.close()
+    cnx.close()
--- write.py
+++ write.py
@@ -1,23 +1,35 @@
-import mysql.connector
+from mysql.connector import errorcode
 import xml.etree.ElementTree as ET
+import mysql.connector
 import dicttoxml
 
 #conexion-datos
-cnx = mysql.connector.connect(host="127.0.0.1", user="root", passwd="12345", db="bd_reto")
-exe = cnx.cursor(dictionary=True)
+try:
+    cnx = mysql.connector.connect(host="127.0.0.1", user="root", passwd="12345", db="bd_reto")
+except mysql.connector.Error as er:
+    if er.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Usuario o contraseña incorrecto.")
+        print('')
+    elif er.errno == errorcode.ER_BAD_DB_ERROR:
+        print("No existe la Base de Datos indicada.")
+        print('')
+    else:
+        print(er)
+else:
+    exe = cnx.cursor(dictionary=True)
 
-#codigos
-query = "SELECT * FROM oc"
-exe.execute(query)
-filas = exe.fetchall()
+    #codigos
+    query = "SELECT * FROM oc"
+    exe.execute(query)
+    filas = exe.fetchall()
 
-cnx.close()
+    cnx.close()
 
-#Archivo XML
-ruta = "E:/C disk/Local C/Documentos/Universidad/5° Semestre/Repos 5°/Bases de datos/proyecto/Reto"
+    #Archivo XML
+    ruta = "E:/C disk/Local C/Documentos/Universidad/5° Semestre/Repos 5°/Bases de datos/proyecto/Reto"
 
-xml = dicttoxml.dicttoxml(filas, custom_root='productos', attr_type=False)
-print(xml)
-tree = ET.ElementTree(ET.fromstring(xml))
+    xml = dicttoxml.dicttoxml(filas, custom_root='productos', attr_type=False)
+    print(xml)
+    tree = ET.ElementTree(ET.fromstring(xml))
 
-tree.write(ruta+'/output.xml', xml_declaration=True)
+    tree.write(ruta+'/output.xml', xml_declaration=True)
