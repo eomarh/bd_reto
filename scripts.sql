017793050b64ee7e9486922a5086ae559bc72521
Omar Herrera <ing.omaar.h@gmail.com>
2020-12-05 11:55:33 -0600

    Scrips DB

---
+++ scripts.sql
@@ -0,0 +1,13 @@
+-- CREATE TABLE IF NOT EXISTS `oc_2` (
+-- `id_oc` int unsigned NOT NULL AUTO_INCREMENT,
+-- `proveedor` varchar(9) DEFAULT NULL,
+-- `producto` varchar(9) DEFAULT NULL,
+-- `descproducto` varchar(25) DEFAULT NULL,
+-- `cantidad` int DEFAULT NULL,
+-- `fechaentrega` date DEFAULT NULL,
+-- `precio` decimal(7,2) DEFAULT NULL,
+-- PRIMARY KEY (`id_oc`)
+-- );
+-- INSERT INTO oc(id_oc, proveedor, producto, descproducto, cantidad, fechaentrega, precio) VALUES(2, 'Puga', 'Refresco', 'Coca-Cola', 15, '2020-09-25', 15.0 )
+-- SELECT * FROM oc_2
+-- DELETE FROM oc_2	
